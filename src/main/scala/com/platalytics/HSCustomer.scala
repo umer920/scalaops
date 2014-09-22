c169bf27812784ebb618d08b1c3a97961a6a6825
umer920 <ufbhatti92@gmail.com>
2014-09-22 14:39:56 +0500

    Purchased Items

---
+++ src/main/scala/com/platalytics/HSCustomer.scala
@@ -0,0 +1,35 @@
+package com.platalytics
+
+import org.apache.spark._
+import org.apache.spark.SparkConf
+//import org.apache.spark.api.java.JavaSparkContext
+import org.apache.spark.SparkContext._
+
+
+class HSCustomer {
+  
+  def HighestSpendingCustomer(args : Array[String]) = {
+    val conf = new SparkConf().setAppName("Data Processor");
+     //conf.set("spark.serializer","org.apache.spark.serializer.KryoSerializer");
+     //conf.set("spark.kryo.registrator", "com.platalytics.spark.MyRegistrator"); 
+     val sc = new SparkContext(conf);  
+     
+     var user_purchases = sc.textFile("Purchases.csv").map(line => {
+      println(line)
+      val parts = line.split(",")
+      (parts(1).toInt, (parts(4).toDouble, parts(5).toDouble))
+     })
+     
+     var user_expenditure = user_purchases.map(f => (f._1 , f._2._1 * f._2._2))
+     var result = user_expenditure.reduceByKey((a, b) => a + b)
+     
+     
+     var res = result.collect.toSeq.sortBy(_._2)
+     res = res.reverse
+     res.foreach(f=>println(f._1 + " , "+f._2))
+     
+     
+     
+  }
+
+}
