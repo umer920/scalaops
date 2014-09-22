160754833b4a58871e22fc5cc97a1c1bd65a501c
umer920 <ufbhatti92@gmail.com>
2014-09-22 13:01:23 +0500

    Purchased Items

--- src/main/scala/com/platalytics/HighestRevenueGen.scala
+++ src/main/scala/com/platalytics/HighestRevenueGen.scala
@@ -22,16 +22,16 @@ class HRGenerator {
      var qty_table = sc.textFile("Purchases.csv").map(line => {
       println(line)
       val parts = line.split(",")
-      (parts(3).toInt, parts(4).toInt)
+      (parts(3).toInt, parts(4).toDouble)
      })
      
      qty_table  = qty_table.reduceByKey((a, b) => a + b)
      
      var highest_revenue = price_table.join(qty_table)
      
-     highest_revenue.map(f => f._2._1 * f._2._2)
+     var revenue = highest_revenue.map(f => (f._1 , f._2._1 * f._2._2))
      
-     var result = highest_revenue.collect.toSeq.sortBy(_._2)
+     var result = revenue.collect.toSeq.sortBy(_._2)
      result = result.reverse
      result.foreach(f=>println(f._1 + " , "+f._2))
      
